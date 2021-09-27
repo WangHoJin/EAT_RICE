55cafe612cc59034e70d046df5bd568a5dc002ac
왕호진 <kkgmco0825@gmail.com>
2021-09-27 17:58:12 +0900

    fix: Delete unnecessary code(#S05P21B303-116)
    Former-commit-id: 4ec29b3efb8d9bbb7d78bcb2e07d0e232d2af11b

--- backend/spring/src/main/java/com/ssafy/spring/controller/UserController.java
+++ backend/spring/src/main/java/com/ssafy/spring/controller/UserController.java
@@ -15,8 +15,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.ws.Response;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
