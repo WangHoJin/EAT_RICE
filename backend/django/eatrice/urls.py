74ba762b027ac3b4024d91835de1bc8ba25742c3
최경운 <mengno5314@naver.com>
2021-10-07 00:59:41 +0900

    fix: Modify API url (#S05P21B303-144)
    Former-commit-id: b1c8fde00b7d2d499e21f261d6847ecc2ad514e6

--- backend/django/eatrice/urls.py
+++ backend/django/eatrice/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/recomm/', include('recomm_api.urls'))
+    path('recomm/', include('recomm_api.urls'))
 ]
